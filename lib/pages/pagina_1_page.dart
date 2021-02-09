6715ac930d32b25bc29bff14e5728028e15fc1fe
Drack <nautilus_com@yahoo.com>
2021-02-08 18:30:40 -0600

    diseño de paginas

--- lib/pages/pagina_1_page.dart
+++ lib/pages/pagina_1_page.dart
@@ -9,9 +9,7 @@ class Pagina1Page extends StatelessWidget {
       appBar: AppBar(
         title: Text('Pagina 1 que hace'),
       ),
-      body: Center(
-        child: Text('Pagina1Page'),
-     ),
+      body: InformacionUsuario(),
      floatingActionButton: FloatingActionButton(
        child: Icon(Icons.account_box),
        onPressed: () {
@@ -19,5 +17,39 @@ class Pagina1Page extends StatelessWidget {
        }
        ),
    );
+  }
+}
+
+class InformacionUsuario extends StatelessWidget {
+  // const InformacionUsuario({
+  //   Key key,
+  // }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          Divider(),
+
+          ListTile(title: Text('Nombre: ')),
+          ListTile(title: Text('Edad: ')),
+
+          Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          Divider(),
+
+          ListTile(title: Text('Profesion 1')),
+          ListTile(title: Text('Profesion 1')),
+          ListTile(title: Text('Profesion 1')),
+
+        ],
+      ),
+    );
   }
 }
--- lib/pages/pagina_2_page.dart
+++ lib/pages/pagina_2_page.dart
@@ -9,9 +9,37 @@ class Pagina2Page extends StatelessWidget {
       appBar: AppBar(
       title: Text('Pagina 2 hola que hace'),
       ),
-      body: Center(
-        child: Text('Pagina2Page'),
-     ),
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MaterialButton(
+              child: Text('Establecer Usuario',style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: (){
+                
+              }
+              ),
+            MaterialButton(
+              child: Text('Edad',style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: (){
+                
+              }
+              ),
+            MaterialButton(
+              child: Text('Añadir profesion',style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: (){
+                
+              }
+              )
+
+              
+          ],
+        )
+      )
+     
    );
   }
 }
